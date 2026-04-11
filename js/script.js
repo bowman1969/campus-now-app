25a0af4e08b89c651c4219264ff01a42c464640e
bowman1969 <bowman.design.pro@gmail.com>
2026-04-11 11:06:45 -0400

    Cleaning up CSS and adding colors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
+const track = document.getElementById("featureTrack");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
-prevBtn.addEventListener("click", () => {
-  console.log("Previous clicked");
-});
+let index = 0;
+const cardWidth = 112; // 100px + gap
+const maxIndex = 5; // 8 items, show 3 → 5 moves
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * cardWidth}px)`;
+}
 
 nextBtn.addEventListener("click", () => {
-  console.log("Next clicked");
+  if (index < maxIndex) {
+    index++;
+    updateCarousel();
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
+
+/* Button sticky active state */
+
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", () => {
+  signupBtn.classList.toggle("active");
 });
